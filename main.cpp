adbc72d308bcd578ca192200b679133fafec6aa2
militrik <militrik@gmail.com>
2023-05-10 00:22:40 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Parametr.hpp"
+
+extern Parametr_t myParams[];
+uint16_t vvv;
+
+int main() {
+//    std::cout << *myParams[1].minValue << std::endl;
+//    std::cout << *myParams[0].maxValue << std::endl;
+    return 0;
+}
